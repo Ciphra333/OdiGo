9ab4b8c7f3e205abd113e0355e0fe240d528926f
Ciphra333 <vadim.kokin.1999@mail.ru>
2021-08-05 22:18:38 +0400

    Адаптация блока Header
    Адаптация блока Header  766 офорфмлена

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@
         }
     };
 }());
+
+// Burger handler
+// Вызываем функцию
+(function () {
+    // Создаем переменную и обращаемся к ней, querySelector ('ищет указанный класс из css')
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    // addEventListener - подвешиваем обработчик события, по клику на бургер применяем к нашему меню(header_nav_active) отвечает за его активное состояние  
+    burgerItem.addEventListener('click', () => {
+        // При клике на header__nav применяется header__Active, сот-но выезжает меню
+        menu.classList.add('header__nav_active');
+    });
+    // Подвешиваем обработчик события, по клику на бургер header__nav_active уезжает за пределы экрана
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+
+}());
